66379e243defcfa1902c3482549af890fe7d8933
abinishifex <abinishifex@gmail.com>
2026-02-18 10:08:32 +0300

    image change

--- src/data/products.js
+++ src/data/products.js
@@ -15,7 +15,7 @@ const products = [
     id: 3,
     name: "Shoes",
     price: 80,
-    image: "https://via.placeholder.com/150"
+    image: "https://www.ubuy.et/en/product/7IA3PZDOM-airpods-3rd-generation-with-lightning-charging-case?srsltid=AfmBOophOxuUF216TLGM4B_ZmfuAde4_k7r3zRS9l0R8WPdQUVgAxdxG"
   }
 ]
 
